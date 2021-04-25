18af09202a17e1f66e64f653366240898207b530
willies952002 <admin@domnian.com>
2021-04-25 11:23:46 -0400

    Prepare for Update - Remapped Edition
    Spigot decided it would be a good time to repackage
    all NMS classes into a more sensible structure

--- scripts/importmcdev.sh
+++ scripts/importmcdev.sh
@@ -12,16 +12,12 @@ workdir=$basedir/Paper/work
 minecraftversion=$(cat $basedir/Paper/work/BuildData/info.json | grep minecraftVersion | cut -d '"' -f 4)
 decompiledir=$workdir/Minecraft/$minecraftversion/spigot
 
-nms="net/minecraft/server"
+nms="net/minecraft"
 export MODLOG=""
 cd $basedir
 
 export importedmcdev=""
 function import {
-    if [ -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/server/$1.java" ]; then
-        echo "ALREADY IMPORTED $1"
-        return 0
-    fi
     export importedmcdev="$importedmcdev $1"
     file="${1}.java"
     target="$basedir/Paper/Paper-Server/src/main/java/$nms/$file"
@@ -30,7 +26,8 @@ function import {
     if [[ ! -f "$target" ]]; then
         export MODLOG="$MODLOG  Imported $file from mc-dev\n";
         echo "$(bashColor 1 32) Copying $(bashColor 1 34)$base $(bashColor 1 32)to$(bashColor 1 34) $target $(bashColorReset)"
-        cp "$base" "$target"
+        mkdir -p "$(dirname "$target")"
+        cp "$base" "$target" || exit 1
     else
         echo "$(bashColor 1 33) UN-NEEDED IMPORT STATEMENT:$(bashColor 1 34) $file $(bashColorReset)"
     fi
@@ -57,17 +54,16 @@ function importLibrary {
 }
 
 (
-    cd Paper/Paper-Server/
+    cd "$basedir/Paper/Paper-Server/"
     lastlog=$(git log -1 --oneline)
     if [[ "$lastlog" = *"PD-Extra mc-dev Imports"* ]]; then
         git reset --hard HEAD^
     fi
 )
 
+files=$(cat "$basedir/patches/server/"* | grep "+++ b/src/main/java/net/minecraft/" | sort | uniq | sed 's/\+\+\+ b\/src\/main\/java\/net\/minecraft\///g')
 
-files=$(cat patches/server/* | grep "+++ b/src/main/java/net/minecraft/server/" | sort | uniq | sed 's/\+\+\+ b\/src\/main\/java\/net\/minecraft\/server\///g' | sed 's/.java//g')
-
-nonnms=$(cat patches/server/* | grep "create mode " | grep -Po "src/main/java/net/minecraft/server/(.*?).java" | sort | uniq | sed 's/src\/main\/java\/net\/minecraft\/server\///g' | sed 's/.java//g')
+nonnms=$(grep -R "new file mode" -B 1 "$basedir/patches/server/" | grep -v "new file mode" | grep -oE --color=none "net\/minecraft\/.*.java" | sed 's/.*\/net\/minecraft\///g')
 function containsElement {
     local e
     for e in "${@:2}"; do
@@ -75,19 +71,23 @@ function containsElement {
     done
     return 1
 }
-
 for f in $files; do
     containsElement "$f" ${nonnms[@]}
     if [ "$?" == "1" ]; then
-        if [ ! -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/server/$f.java" ]; then
+        if [ ! -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/$f" ]; then
+        f="$(echo "$f" | sed 's/.java//g')"
             if [ ! -f "$decompiledir/$nms/$f.java" ]; then
                 echo "$(bashColor 1 31) ERROR!!! Missing NMS$(bashColor 1 34) $f $(bashColorReset)";
+                error=true
             else
                 import $f
             fi
         fi
     fi
 done
+if [ -n "$error" ]; then
+    exit 1
+fi
 
 ###############################################################################################
 ###############################################################################################
