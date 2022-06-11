2b8ec8b6471e35e5cc64144fc5c3d177f3cc3b2c
willies952002 <admin@domnian.com>
2022-06-11 15:55:00 -0400

    Setup for 1.19 Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,18 +6,20 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.5"
+    id("io.papermc.paperweight.patcher") version "1.3.6"
 }
+
+val paperRepoUrl = "https://papermc.io/repo/repository/maven-public/";
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven(paperRepoUrl) {
         content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.1:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.605.7")
     paperclip("io.papermc:paperclip:3.0.2")
 }
@@ -53,8 +55,8 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven(paperRepoUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
@@ -65,8 +67,8 @@ subprojects {
 paperweight {
     serverProject.set(project(":paperdragon-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set(paperRepoUrl)
+    decompileRepo.set(paperRepoUrl)
 
     usePaperUpstream(providers.gradleProperty("paperRef")) {
         withPaperPatcher {
@@ -85,8 +87,7 @@ tasks.generateDevelopmentBundle {
     libraryRepositories.addAll(
         "https://repo.maven.apache.org/maven2/",
         "https://libraries.minecraft.net/",
-        "https://papermc.io/repo/repository/maven-public/",
-        "https://maven.fabricmc.net/",
+        paperRepoUrl
     )
 }
 
